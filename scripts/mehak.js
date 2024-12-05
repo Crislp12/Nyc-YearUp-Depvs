80b6840e145d771ae739e0e6fc21b9562f062970
Mehak Sadiq Ali <sadiqalimehak@gmail.com>
2024-12-05 10:58:45 -0500

    filtering

---
+++ scripts/mehak.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let shopByTypeSelect = document.querySelector("#shopByTypeSelect");
+let categorySelect = document.querySelector("#categorySelect");
+async function getCategories() {
+    try {
+      let url = "http://localhost:8081/api/categories";
+      let response = await fetch(url);
+      let categories = await response.json();
+      console.log("categories", categories);
+      return categories;
+    } catch (error) {
+      console.log("error:", error.message);
+    }
+  }
+  
+  function populateCategorySelect(categories) {
+    categories.forEach((category) => {
+      let option = document.createElement("option");
+      option.value = category.categoryId;
+      option.innerText = category.name;
+      categorySelect.appendChild(option);
+    });
+  }
